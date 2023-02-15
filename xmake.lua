d3c5e05d80ae3f9262c36c66c9ee8a5afe76876e
Arthur Vasseur <arthur.vasseur@epitech.eu>
2023-02-15 12:03:53 +0900

    feat: auto downloading crytek sponza scene

--- xmake.lua
+++ xmake.lua
@@ -18,9 +18,30 @@ target("ConcertoGraphics")
     add_files('shaders/*.vert', 'shaders/*.frag')
     add_packages('glslang')
     after_build(function (target)
-        print("Copying shaders to build directory...")
+        import("net.http")
+        import("utils.archive")
+
         local binaryPath = "$(buildir)/$(plat)/$(arch)/$(mode)"
         os.mkdir(binaryPath .. "/assets")
-        os.cp("assets/**",  binaryPath .. "/assets")
+
+        print("Copying shaders to build directory...")
+        os.cp("shaders/**",  binaryPath .. "/shaders")
         print("Copying shaders to build directory... Done !")
+
+        if (not os.exists("assets/sponza")) then
+            print("Downloading assets...")
+            http.download("https://casual-effects.com/g3d/data10/common/model/crytek_sponza/sponza.zip", "assets/sponza.zip")
+            print("Downloading assets... Done !")
+
+            print("Extracting assets...")
+                archive.extract("assets/sponza.zip", "assets/sponza")
+                os.rm("assets/sponza.zip")
+            print("Extracting assets... Done !")
+        else
+            print("Assets already downloaded !")
+        end
+
+        print("Copying assets to build directory...")
+        os.cp("assets/**",  binaryPath .. "/assets")
+        print("Copying assets to build directory... Done !")
     end)
