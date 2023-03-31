ee9ffac3bf92dcf22252f1a3fe63dcf7f390477e
Arthur Vasseur <arthur.vasseur@epitech.eu>
2023-03-31 17:04:09 +0900

    feat: using free fly camera

--- Examples/BasicRendering/main.cpp
+++ Examples/BasicRendering/main.cpp
@@ -1,13 +1,13 @@
 //
 // Created by arthur on 12/07/2022.
 //
-#include <iostream>
 #include <chrono>
 #include "Concerto/Core/Math/Vector.hpp"
-#include "glm/glm.hpp"
 #include "glm/gtc/quaternion.hpp"
 #include "VulkanRenderer.hpp"
-#include "CameraViewer.hpp"
+#include "Camera.hpp"
+#include <Concerto/Core/Math/Algorithm.hpp>
+#include "Input.hpp"
 
 using namespace Concerto;
 using namespace Concerto::Graphics;
@@ -21,88 +21,113 @@ int main()
 		.width = 1280,
 		.height = 720
 	};
+	Input input;
 	GLFW3WindowPtr window = std::make_unique<GlfW3>(info.applicationName, 1280, 720);
 	VulkanRenderer engine(std::move(info), *window.get());
 	Concerto::Graphics::ImGUI* imGui = engine.GetImGUIContext();
 	assert(imGui != nullptr);
-	ImGuiIO& io = ImGui::GetIO();
 	ImGui::SetCurrentContext(imGui->GetContext());
-	float fractionChangeX;
-	float fractionChangeY;
 	float aspect = float(window->GetWidth()) / float(window->GetHeight());
-	double mouseX, mouseY;
-	bool leftMouseButtonPressed = false;
-	bool rightMouseButtonPressed = false;
-	bool middleMouseButtonPressed = false;
-	double cx = 0;
-	double cy = 0;
-	CameraViewer cameraViewer({ 5.f, 5.f, 5.f }, { 0.f, 0.f, 0.f }, { 0.0f, 1.0f, 0.0f }, 45.f, aspect);
+	float deltaTime = 0.f;
 	Scene sceneParameters{};
 	sceneParameters.gpuSceneData.sunlightDirection = { 3.1f, 1.f, -1.f, 0 };
 	sceneParameters.gpuSceneData.ambientColor = { 0.f, 0.f, 0.f, 1.f };
 	sceneParameters.gpuSceneData.sunlightColor = { 255.f, 109.f, 39.f, 1.f };
 	sceneParameters.clearColor = { 0.1f, 0.1f, 0.1f, 1.f };
 	engine.UpdateSceneParameters(sceneParameters);
+
+	Camera camera(90.f, 0.1f, 1000000.f, aspect);
+
 	window->RegisterCursorPosCallback([&](AWindow<GLFWwindow>& window, double x, double y)
 	{
 	  imGui->UpdateMousePosition(x, y);
-	  cx = x;
-	  cy = y;
+	  Concerto::MouseEvent mouseEvent{};
+	  static double deltaX = 0, deltaY = 0;
+	  deltaX = x - deltaX;
+	  deltaY = y - deltaY;
+	  mouseEvent.type = Concerto::MouseEvent::Moved;
+	  mouseEvent.mouseMove.x = x;
+	  mouseEvent.mouseMove.y = y;
+	  mouseEvent.mouseMove.deltaX = deltaX;
+	  mouseEvent.mouseMove.deltaY = deltaY;
+	  input.TriggerMouseEvent(mouseEvent, deltaTime);
+	  deltaX = x;
+	  deltaY = y;
 	});
+
 	window->RegisterMouseButtonCallback([&](AWindow<GLFWwindow>& window, int button, int action, int mods)
 	{
 	  imGui->UpdateMouseButton(button, action == GLFW_PRESS);
-	  leftMouseButtonPressed = action == GLFW_PRESS && button == GLFW_MOUSE_BUTTON_LEFT;
-	  rightMouseButtonPressed = action == GLFW_PRESS && button == GLFW_MOUSE_BUTTON_RIGHT;
-	  middleMouseButtonPressed = action == GLFW_PRESS && button == GLFW_MOUSE_BUTTON_MIDDLE;
+	  Concerto::MouseEvent mouseEvent{};
+	  mouseEvent.type = Concerto::MouseEvent::Button;
+	  mouseEvent.button.button = Concerto::MouseButton::Button(button);
+	  mouseEvent.button.triggerType = Concerto::TriggerType(action);
+	  input.TriggerMouseEvent(mouseEvent, deltaTime);
 	});
 
-	Camera camera
-		{
-			.view = glm::lookAt(glm::vec3(5.f, 5.f, 5.f), glm::vec3(0.0f, 0.0f, 0.0f),
-				glm::normalize(glm::vec3(0.0f, 1.0f, 0.0f))),
-			.proj = glm::perspective(glm::radians(90.f), aspect, 0.00001f, 10000.0f),
-			.viewproj = camera.proj * camera.view * glm::mat4(1.f)
-		};
+	window->RegisterKeyCallback([&](AWindow<GLFWwindow>& window, Key key, int scancode, int action, int mods)
+	{
+	  Concerto::KeyEvent keyEvent{};
+	  keyEvent.key = Concerto::Key(key);
+	  keyEvent.triggerType = Concerto::TriggerType(action);
+	  input.TriggerKeyEvent(keyEvent, deltaTime);
+	});
+
 	window->RegisterResizeCallback([&](AWindow<GLFWwindow>& window)
 	{
 	  aspect = float(window.GetWidth()) / float(window.GetHeight());
-	  camera.proj = glm::perspective(glm::radians(90.f), aspect, 0.00001f, 10000.0f);
-	  camera.viewproj = camera.proj * camera.view * glm::mat4(1.f);
+	  camera.projectionMatrix = glm::perspective(45.f, aspect, 0.1f, 1000.f);
+	  camera.viewProjectionMatrix = camera.projectionMatrix * camera.viewMatrix * glm::mat4(1.f);
 	});
+
+	bool cursorDisabled = false;
+	window->SetCursorDisabled(cursorDisabled);
+	float speed = 150.f;
+	bool imguiFocused = false;
+	Input::Instance().Register("MouseMoved", MouseEvent::Type::Moved, [&](const MouseEvent& e, float deltaTime)
+	{
+	  if (imguiFocused)
+		  return;
+	  camera.Rotate(e.mouseMove.deltaX, -e.mouseMove.deltaY);
+	});
+
+	Input::Instance().Register("Forward", Key::Z, TriggerType::Held, [&](float deltaTime)
+	{
+	  camera.Move(FreeFlyCameraMovement::Forward, deltaTime * speed);
+	});
+
+	Input::Instance().Register("Backward", Key::S, TriggerType::Held, [&](float deltaTime)
+	{
+	  camera.Move(FreeFlyCameraMovement::Backward, deltaTime * speed);
+	});
+
+	Input::Instance().Register("Left", Key::Q, TriggerType::Held, [&](float deltaTime)
+	{
+	  camera.Move(FreeFlyCameraMovement::Left, deltaTime * speed);
+	});
+
+	Input::Instance().Register("_right", Key::D, TriggerType::Held, [&](float deltaTime)
+	{
+	  camera.Move(FreeFlyCameraMovement::Right, deltaTime * speed);
+	});
+
+	Input::Instance().Register("MouseFocused", Key::LeftAlt, TriggerType::Pressed, [&](float deltaTime)
+	{
+	  cursorDisabled = !cursorDisabled;
+	  window->SetCursorDisabled(cursorDisabled);
+	});
+
 	MeshPtr sponzaMesh = std::make_shared<Mesh>();
 	sponzaMesh->LoadFromFile("./assets/sponza/sponza.obj");
-	std::chrono::steady_clock::time_point lastFrameTime = std::chrono::steady_clock::now();
-	float deltaTime = 0.f;
 	Math::Vector3f position(0.f, 0.f, 0.f);
 	Math::Quaternionf rotation(Math::EulerAnglesf(0.f, 0.f, 0.f));
-	Math::Vector3f scale(0.1f, 0.1f, 0.1f);
+	Math::Vector3f scale(1.f, 1.f, 1.f);
+
+	std::chrono::steady_clock::time_point lastFrameTime = std::chrono::steady_clock::now();
 	while (!window->ShouldClose())
 	{
 		window->PopEvent();
-		if (!ImGui::IsWindowFocused(ImGuiFocusedFlags_AnyWindow))
-		{
-			if (leftMouseButtonPressed)
-			{
-				fractionChangeX = static_cast<float>(cx - mouseX) / static_cast<float>(window->GetWidth());
-				fractionChangeY = static_cast<float>(mouseY - cy) / static_cast<float>(window->GetHeight());
-				cameraViewer.Rotate(fractionChangeX, fractionChangeY);
-			}
-			if (rightMouseButtonPressed)
-			{
-				fractionChangeX = static_cast<float>(cx - mouseX) / static_cast<float>(window->GetWidth());
-				fractionChangeY = static_cast<float>(mouseY - cy) / static_cast<float>(window->GetHeight());
-				cameraViewer.Translate(fractionChangeX, fractionChangeY);
-			}
-			if (middleMouseButtonPressed)
-			{
-				fractionChangeY = static_cast<float>(mouseY - cy) / static_cast<float>(window->GetHeight());
-				cameraViewer.Zoom(fractionChangeY);
-			}
-			cameraViewer.UpdateCameraView(camera);
-			mouseX = cx;
-			mouseY = cy;
-		}
+		imguiFocused = ImGui::IsWindowFocused(ImGuiFocusedFlags_AnyWindow);
 		auto beginTime = std::chrono::high_resolution_clock::now();
 		deltaTime = std::chrono::duration<float>(beginTime - lastFrameTime).count();
 		lastFrameTime = beginTime;
@@ -115,18 +140,26 @@ int main()
 		ImGui::Text("Draw %f ms", engine._drawTime * 1000.f);
 		ImGui::Text("Load meshes %f ms", engine._drawObjectsTime * 1000.f);
 		ImGui::End();
+
+		ImGui::Begin("Camera");
+		ImGui::Text("Position : %f %f %f",
+			camera.GetPosition().X(),
+			camera.GetPosition().Y(),
+			camera.GetPosition().Z());
+		ImGui::Text("Rotation : %f %f %f",
+			camera.GetRotation().Pitch(),
+			camera.GetRotation().Yaw(),
+			camera.GetRotation().Roll());
+		ImGui::DragFloat("Speed", &speed, 1.f, 0.f, 100000.f);
+		ImGui::End();
+
 		if (ImGui::ColorEdit3("Select clear color", &sceneParameters.clearColor[0]))
-		{
 			engine.UpdateSceneParameters(sceneParameters);
-		}
 		if (ImGui::ColorEdit3("Select ambiant color", &sceneParameters.gpuSceneData.ambientColor[0]))
-		{
 			engine.UpdateSceneParameters(sceneParameters);
-		}
 		if (ImGui::ColorEdit3("Select sun color", &sceneParameters.gpuSceneData.sunlightColor[0]))
-		{
 			engine.UpdateSceneParameters(sceneParameters);
-		}
+
 		engine.DrawObject(sponzaMesh, position, rotation, scale);
 		engine.Draw(camera);
 	}
