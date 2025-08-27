f2130522154188f58eb659d2c500c612d39a8321
Arthur Vasseur <arthur.vasseur@hotmail.fr>
2025-08-27 19:37:34 +0200

    feat: add default constructor and Create method to DescriptorPool; enhance error handling with VkException

--- Src/Backend/Vulkan/Wrapper/DescriptorPool.cpp
+++ Src/Backend/Vulkan/Wrapper/DescriptorPool.cpp
@@ -8,39 +8,29 @@
 
 #include <Concerto/Core/Assert.hpp>
 
+#include "Concerto/Graphics/Backend/Vulkan/VkException.hpp"
 #include "Concerto/Graphics/Backend/Vulkan/Wrapper/Device.hpp"
 
 namespace cct::gfx::vk
 {
 	DescriptorPool::DescriptorPool(Device& device) : Object(device)
 	{
-		std::vector<VkDescriptorPoolSize> sizes =
+		std::vector<VkDescriptorPoolSize> poolSizes =
 		{
-				{ VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,         1000 },
-				{ VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, 1000 },
-				{ VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,         1000 },
-				{ VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1000 }
+				{.type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, .descriptorCount = 1000 },
+				{.type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, .descriptorCount = 1000 },
+				{.type = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, .descriptorCount = 1000 },
+				{.type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, .descriptorCount = 1000 }
 		};
-		VkDescriptorPoolCreateInfo descriptorPoolCreateInfo = {};
-		descriptorPoolCreateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
-		descriptorPoolCreateInfo.maxSets = 1000;
-		descriptorPoolCreateInfo.poolSizeCount = static_cast<UInt32>(sizes.size());
-		descriptorPoolCreateInfo.pPoolSizes = sizes.data();
-		descriptorPoolCreateInfo.flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT;
-		m_lastResult = device.vkCreateDescriptorPool(*device.Get(), &descriptorPoolCreateInfo, nullptr, &m_handle);
-		CCT_ASSERT(m_lastResult == VK_SUCCESS, "ConcertoGraphics: vkCreateDescriptorPool failed VkResult={}", static_cast<const int>(m_lastResult));
+
+		if (Create(device, std::move(poolSizes)) != VK_SUCCESS)
+			throw VkException(GetLastResult());
 	}
 
 	DescriptorPool::DescriptorPool(Device& device, std::vector<VkDescriptorPoolSize> poolSizes) : Object<VkDescriptorPool>(device)
 	{
-		VkDescriptorPoolCreateInfo descriptorPoolCreateInfo = {};
-		descriptorPoolCreateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
-		descriptorPoolCreateInfo.maxSets = 10;
-		descriptorPoolCreateInfo.poolSizeCount = static_cast<UInt32>(poolSizes.size());
-		descriptorPoolCreateInfo.pPoolSizes = poolSizes.data();
-		descriptorPoolCreateInfo.flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT;
-		m_lastResult = device.vkCreateDescriptorPool(*device.Get(), &descriptorPoolCreateInfo, nullptr, &m_handle);
-		CCT_ASSERT(m_lastResult == VK_SUCCESS, "ConcertoGraphics: Failed to create descriptor pool VkResult={}", static_cast<const int>(m_lastResult));
+		if (Create(device, std::move(poolSizes)) != VK_SUCCESS)
+			throw VkException(GetLastResult());
 	}
 
 	DescriptorPool::~DescriptorPool()
@@ -50,13 +40,34 @@ namespace cct::gfx::vk
 		m_device->vkDestroyDescriptorPool(*m_device->Get(), m_handle, nullptr);
 	}
 
+	VkResult DescriptorPool::Create(Device& device, std::vector<VkDescriptorPoolSize> poolSizes)
+	{
+		m_device = &device;
+
+		VkDescriptorPoolCreateInfo descriptorPoolCreateInfo = {};
+		descriptorPoolCreateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+		descriptorPoolCreateInfo.maxSets = 1000;
+		descriptorPoolCreateInfo.poolSizeCount = static_cast<UInt32>(poolSizes.size());
+		descriptorPoolCreateInfo.pPoolSizes = poolSizes.data();
+		descriptorPoolCreateInfo.flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT;
+
+		m_lastResult = device.vkCreateDescriptorPool(*device.Get(), &descriptorPoolCreateInfo, nullptr, &m_handle);
+		CCT_ASSERT(m_lastResult == VK_SUCCESS, "ConcertoGraphics: vkCreateDescriptorPool failed VkResult={}", static_cast<const int>(m_lastResult));
+
+		return m_lastResult;
+	}
+
 	DescriptorSet DescriptorPool::AllocateDescriptorSet(DescriptorSetLayout& setLayout)
 	{
+		CCT_ASSERT(!IsNull(), "Invalid object state, 'Create' must be called");
+
 		return { *m_device, *this, setLayout };
 	}
 
-	void DescriptorPool::Reset()
+	void DescriptorPool::Reset() const
 	{
+		CCT_ASSERT(!IsNull(), "Invalid object state, 'Create' must be called");
+
 		m_device->vkResetDescriptorPool(*m_device->Get(), m_handle, 0);
 	}
 }
