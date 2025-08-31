2e33155654482296c2726b223a69f6155e709030
Arthur Vasseur <arthur.vasseur@hotmail.fr>
2025-08-31 13:05:37 +0200

    fix: add_rpathdirs("$ORIGIN")

--- Examples/RHI/xmake.lua
+++ Examples/RHI/xmake.lua
@@ -5,6 +5,8 @@ target('concerto-rhi-sample')
     set_warnings('allextra')
     add_rules('download.assets', 'compile.shaders')
     add_packages("concerto-core", { public = true })
+    add_rpathdirs("$ORIGIN")
+    
     if has_config("profiling") then
         add_deps("concerto-profiler", { public = false })
     end
